6a9b7f31ee0ea7eb835553963ad3ae106a7a918e
iam-sphinx <kandarp1803@gmail.com>
2024-02-19 00:34:52 +0530

    testing

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,15 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://donut-a-blog-website.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 // import routes
