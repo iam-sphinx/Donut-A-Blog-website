1b3db0282250d96b5bf904fe595dbe4ffb5354cd
iam-sphinx <kandarp1803@gmail.com>
2024-04-04 04:33:01 +0530

    done

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 app.use(
   cors({
-    origin: "https://donut-a-blog-website.vercel.app/",
+    origin: "https://donut-a-blog-website.vercel.app",
     credentials: true,
   })
 );
